8a45f223a5b06ecf7d23eda13ffc02cc17f71172
K0LESAN <k0lesan2k00@gmail.com>
2025-01-04 19:04:50 +0300

    chore: add webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+let mode = 'development';
+
+if (process.env.NODE_ENV === 'production') {
+  mode = 'production';
+}
+
+module.exports = {
+  mode,
+  entry: {
+    app: path.join(__dirname, 'src', 'index.tsx'),
+  },
+  devServer: {
+    static: './dist',
+    hot: true,
+  },
+  target: 'web',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'index.html')
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [
+          mode === 'development' ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|webp)$/i,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.ttf$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.svg$/i,
+        type: 'asset/source'
+      }
+    ],
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  }
+}
