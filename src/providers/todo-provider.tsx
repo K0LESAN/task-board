13317e2af64f3235f3ab4b47fc76f22beacdcabc
K0LESAN <k0lesan2k00@gmail.com>
2025-01-05 17:33:05 +0300

    feat: memoize actions

--- src/providers/todo-provider.tsx
+++ src/providers/todo-provider.tsx
@@ -1,6 +1,11 @@
 import type { Todo, TodoContext } from '@/types';
 import { TodoType } from '@/constants';
-import { createContext, useState, type PropsWithChildren } from 'react';
+import {
+  type PropsWithChildren,
+  createContext,
+  useCallback,
+  useState,
+} from 'react';
 
 export const todoContext = createContext<TodoContext>({
   todos: [],
@@ -14,56 +19,68 @@ export const todoContext = createContext<TodoContext>({
 
 const TodoProvider = ({ children }: PropsWithChildren) => {
   const [todos, setTodos] = useState<Todo[]>([]);
-  const initTodos = (newTodos: Todo[]): void => {
+  const initTodos = useCallback((newTodos: Todo[]): void => {
     setTodos(newTodos);
-  };
-  const addTodo = (newTodo: Omit<Todo, 'id' | 'type'>): void => {
-    const maxId: number = todos.reduce(
-      (accMaxId: number, { id }: Todo): number => {
-        return accMaxId > id ? accMaxId : id;
-      },
-      0
-    );
-
-    setTodos((prev: Todo[]): Todo[] => {
-      return [
-        ...prev,
-        {
-          id: maxId + 1,
-          type: TodoType.todo,
-          ...newTodo,
+  }, []);
+  const addTodo = useCallback(
+    (newTodo: Omit<Todo, 'id' | 'type'>): void => {
+      const maxId: number = todos.reduce(
+        (accMaxId: number, { id }: Todo): number => {
+          return accMaxId > id ? accMaxId : id;
         },
-      ];
-    });
-  };
-  const removeTodo = (id: number): void => {
-    const newTodos = todos.filter(({ id: todoId }: Todo): boolean => {
-      return todoId !== id;
-    });
+        0
+      );
 
-    if (todos.length !== newTodos.length) {
+      setTodos((prev: Todo[]): Todo[] => {
+        return [
+          ...prev,
+          {
+            id: maxId + 1,
+            type: TodoType.todo,
+            ...newTodo,
+          },
+        ];
+      });
+    },
+    [todos]
+  );
+  const removeTodo = useCallback(
+    (id: number): void => {
+      const newTodos = todos.filter(({ id: todoId }: Todo): boolean => {
+        return todoId !== id;
+      });
+
+      if (todos.length !== newTodos.length) {
+        setTodos(newTodos);
+      }
+    },
+    [todos]
+  );
+  const changeTodo = useCallback(
+    (newTodo: Todo): void => {
+      const newTodos = todos.filter(({ id: todoId }: Todo): boolean => {
+        return todoId !== newTodo.id;
+      });
+
+      if (todos.length !== newTodos.length) {
+        setTodos([...newTodos, newTodo]);
+      }
+    },
+    [todos]
+  );
+  const clearTodosByType = useCallback(
+    (clearType: TodoType): void => {
+      const newTodos = todos.filter(({ type }: Todo): boolean => {
+        return clearType !== type;
+      });
+      console.log(todos, clearType);
       setTodos(newTodos);
-    }
-  };
-  const changeTodo = (newTodo: Todo): void => {
-    const newTodos = todos.filter(({ id: todoId }: Todo): boolean => {
-      return todoId !== newTodo.id;
-    });
-
-    if (todos.length !== newTodos.length) {
-      setTodos([...newTodos, newTodo]);
-    }
-  };
-  const clearTodosByType = (clearType: TodoType): void => {
-    const newTodos = todos.filter(({ type }: Todo): boolean => {
-      return clearType !== type;
-    });
-
-    setTodos(newTodos);
-  };
-  const clearTodos = (): void => {
+    },
+    [todos]
+  );
+  const clearTodos = useCallback((): void => {
     setTodos([]);
-  };
+  }, [todos]);
 
   return (
     <todoContext.Provider
